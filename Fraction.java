2dedf5150261be100f7f2710aa0d6959ba717b7f
stwaltz <145263576+stwaltz@users.noreply.github.com>
2024-02-17 13:00:09 +0800

    Update Fraction.java

--- Fraction.java
+++ Fraction.java
@@ -1,2 +1,29 @@
+package FractionsP2;
 public class Fraction {
+    private int numerator;
+    private int denominator;
+    public Fraction(){ // no cons
+     int numerator = 0;
+     int denominator = 1;
+    }
+    public Fraction(int numerator, int denominator){
+        setNumerator(numerator);
+        setDenominator(denominator);
+    }
+    public void setNumerator(int num) {
+        numerator = num;
+    }
+    public int getNumerator() {
+        return numerator;
+    }
+    public void setDenominator(int den) {
+        denominator = den;
+    }
+    public int getDenominator() {
+        return denominator;
+    }
+  // add other 
 }
+
+
+
