ca0f50ef7591da04fefa553d836f576f67df0c65
Henri Agustin <147720221+illogicbit@users.noreply.github.com>
2024-02-17 11:11:14 +0800

    Add files via upload

--- Fraction.java
+++ Fraction.java
@@ -1,2 +1,16 @@
 public class Fraction {
+
+    //Returns the product fraction of this fraction and a multiplier fraction.
+    public Fraction multiplyBy(Fraction multiplier) {
+        return new Fraction(
+                this.numerator * multiplier.getNumerator(),
+                this.denominator * multiplier.getNumerator()
+        );
+    }
+
+    //Returns a string representation of this fraction
+    //Format of string representation is "[numerator]/[denominator]"
+    public String toString(){
+        return String.format("%d/%d",this.numerator,this.denominator);
+    }
 }
